7551413f21aa9017de2b8ae70bff9e2ea9e7a7a3
indrajit-28 <indrajitsinh.rathod-i@equestsolutions.net>
2025-02-24 10:33:40 +0530

    first commit

--- backend/dbconnection/connection.js
+++ backend/dbconnection/connection.js
@@ -1,6 +1,6 @@
 const mongoose =require("mongoose")
 
-mongoose.connect("mongodb+srv://wayseasy291:N5va3DXwRiGlHckz@cluster0.8uqrqj7.mongodb.net/digitalmoneyapp?retryWrites=true&w=majority").then(()=>{
+mongoose.connect("mongodb+srv://rathodindrajit:rathodindrajit@cluster0.yfadb.mongodb.net/digitalmoney?retryWrites=true&w=majority&appName=Cluster0").then(()=>{
     console.log("connection succesfull")
 }).catch((e)=>{
     console.log(e)
